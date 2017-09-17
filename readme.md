59507bdaadd6ad22145519c3965a26b0a9d907d4
cu <gitcub2@cusp.de>
2017-09-17 02:29:55 +0200

    init

---
+++ readme.md
@@ -0,0 +1,36 @@
+# CurNotesMDs - IntelliJ - Plugin
+
+IntelliJ (and all intellj based IDEs) from 
+jetbrains can use plugins to expand the range of functions.
+
+This is a Plugin for all intellj-based IDEs.
+
+## What can I do with this ?
+
+With this plugin, you can save some notes to your project on an very easy way.
+
+It's markdown-format - so you can ease format your notes. 
+
+The notes will be stored in .idea folder - so normally they will not be 
+tracked by a version system (like git), if you exclude your .idea -folder from git.
+
+If you **want** to be your notes to be tracked by git, you can add this line into your .gitignore file:
+
+!.idea/.curNotes.md
+
+The plugin requires the MarkDownSupport Plugin 
+
+If you don't want to changes in markdown-format, you can use the Plugion
+
+CurNotes instead of this (CurNotesMDs).
+
+[https://plugins.jetbrains.com/plugin/edit?pluginId=10017](https://plugins.jetbrains.com/plugin/edit?pluginId=10017)
+
+Jörg Wrase
+
+[www.cusp.de](https://www.cusp.de) 
+
+ 
+
+
+
---
+++ src/CurNotesStart.java
@@ -0,0 +1,76 @@
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.vfs.LocalFileSystem;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ *
+ */
+public class CurNotesStart extends AnAction {
+
+    private static String curNotesFileName = ".curNotes.md";
+    private Project project;
+
+    @Override
+    public void actionPerformed(AnActionEvent event) {
+
+        VirtualFile fileV;
+
+        project = event.getData(PlatformDataKeys.PROJECT);
+
+        fileV = buildVirtualFile();
+
+        if (fileV != null) {
+            FileEditorManager.getInstance(project).openFile(fileV, false);
+        }
+
+    }
+
+    /**
+     * @return String
+     */
+    @NotNull
+    private String getFilePath() {
+        return this.project.getBasePath() + File.separator + Project.DIRECTORY_STORE_FOLDER + File.separator + CurNotesStart.curNotesFileName;
+    }
+
+    /**
+     * @return VirtualFile
+     */
+    private VirtualFile buildVirtualFile() {
+
+        Boolean success = true;
+        String filePath = getFilePath();
+        File file = new File(filePath);
+        VirtualFile fileVirtual = null;
+
+        if (!file.exists()) {
+            try {
+                success = file.createNewFile();
+            } catch (IOException e) {
+                success = false;
+            }
+        }
+
+        if (success) {
+            fileVirtual = LocalFileSystem.getInstance().refreshAndFindFileByIoFile(file);
+        }
+
+        String errorMessage = "An error has occurred. The VirtualFile could not be created. Please try again.";
+
+        if (fileVirtual == null) {
+            Messages.showMessageDialog(project, errorMessage, "Error", Messages.getErrorIcon());
+        }
+
+        return fileVirtual;
+    }
+
+}
